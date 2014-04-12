001e702ce64a64537e85600914a7d07a8f79216a
Edward Thomson <ethomson@edwardthomson.com>
2014-04-11 19:16:06 -0500

    chains

--- tests/core/link.c
+++ tests/core/link.c
@@ -287,6 +287,20 @@ void test_core_link__stat_symlink_directory(void)
 	cl_assert(S_ISDIR(st.st_mode));
 }
 
+void test_core_link__stat_symlink_chain(void)
+{
+	struct stat st;
+
+	cl_git_rewritefile("stat_final_target", "Final target of some symbolic links...\n");
+	do_symlink("stat_final_target", "stat_chain_3", 0);
+	do_symlink("stat_chain_3", "stat_chain_2", 0);
+	do_symlink("stat_chain_2", "stat_chain_1", 0);
+
+	cl_must_pass(p_stat("stat_chain_1", &st));
+	cl_assert(S_ISREG(st.st_mode));
+	cl_assert_equal_i(39, st.st_size);
+}
+
 void test_core_link__stat_dangling_symlink(void)
 {
 	struct stat st;
