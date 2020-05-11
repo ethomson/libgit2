cb4f75a943db316e693fbff963d14369765a5b5a
Edward Thomson <ethomson@edwardthomson.com>
2020-05-11 14:38:08 +0100

    fixup git2client.h

--- include/git2client.h
+++ include/git2client.h
@@ -8,6 +8,7 @@
 #ifndef INCLUDE_git_client_h__
 #define INCLUDE_git_client_h__
 
+#include <git2.h>
 #include "git2client/exec_filter.h"
 #include "git2client/global.h"
 #include "git2client/ssh_subtransport.h"
