feab4fce4623c58050ac74e99105d4da6a02a625
Edward Thomson <ethomson@edwardthomson.com>
2022-02-18 09:17:09 -0500

    ubuntu

--- ci/setup-ubuntu-benchmark.sh
+++ ci/setup-ubuntu-benchmark.sh
@@ -2,5 +2,17 @@
 
 set -x
 
-sudo apt-get install cargo
+sudo apt-get update
+sudo apt-get install -y --no-recommends \
+	cargo \
+        cmake \
+        gcc \
+        git \
+        krb5-user \
+        libkrb5-dev \
+        libssl-dev \
+        libz-dev \
+        make \
+        ninja-build \
+        pkgconf
 cargo install hyperfine
