c7ebda88086381b714757643d44d04a0c01aa41c
Edward Thomson <ethomson@edwardthomson.com>
2022-02-20 15:27:40 +0000

    nice

--- tests/benchmarks/benchmark_helpers.sh
+++ tests/benchmarks/benchmark_helpers.sh
@@ -21,11 +21,12 @@ NEXT=
 BASELINE_CLI=
 TEST_CLI="git"
 JSON=
+SHOW_OUTPUT=
 
 if [ "$CI" != "" ]; then
-	OUTPUT_STYLE="color"
+	OUTPUT_STYLE="--style color"
 else
-	OUTPUT_STYLE="auto"
+	OUTPUT_STYLE="--style auto"
 fi
 
 for a in "$@"; do
@@ -55,6 +56,9 @@ for a in "$@"; do
 		NEXT="json"
 	elif [[ "${a}" == "-j"* ]]; then
 		JSON="${a}"
+	elif [ "${a}" = "--show-output" ]; then
+		SHOW_OUTPUT="--show-output"
+		OUTPUT_STYLE=
 	fi
 done
 
@@ -68,7 +72,20 @@ NAME=$(basename $0)
 create_rand() {
 	KILOBYTES=${1:=1}
 	FILENAME=$(mktemp)
-	echo "${FILENAME}"
+
+	if [[ "$(uname -s)" == "MINGW"* ]]; then
+		DEVICE="/dev/random"
+	else
+		DEVICE="/dev/urandom"
+	fi
+
+	dd if="${DEVICE}" of="${FILENAME}" bs=1k count="${KILOBYTES}" >/dev/null 2>&1
+
+	if [[ "$(uname -s)" == "MINGW"* ]]; then
+		cygpath -w "${FILENAME}"
+	else
+		echo "${FILENAME}"
+	fi
 }
 
 flush_cache() {
@@ -96,8 +113,8 @@ gitbench() {
 	fi
 
 	if [ "${BASELINE_CLI}" != "" ]; then
-		hyperfine --prepare "${PREPARE}" --warmup "${WARMUP}" --style "${OUTPUT_STYLE}" ${JSON_ARG} "${BASELINE_CLI} ${1}" "${TEST_CLI} ${1}"
+		hyperfine --prepare "${PREPARE}" --warmup "${WARMUP}" ${OUTPUT_STYLE} ${SHOW_OUTPUT} ${JSON_ARG} "${BASELINE_CLI} ${1}" "${TEST_CLI} ${1}"
 	else
-		hyperfine --prepare "${PREPARE}" --warmup "${WARMUP}" --style "${OUTPUT_STYLE}" ${JSON_ARG} "${TEST_CLI} ${1}"
+		hyperfine --prepare "${PREPARE}" --warmup "${WARMUP}" ${OUTPUT_STYLE} ${SHOW_OUTPUT} ${JSON_ARG} "${TEST_CLI} ${1}"
 	fi
 }
