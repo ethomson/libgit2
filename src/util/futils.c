f18b943fe4d4b9ef32ad76ce8fc618e4a30c4cb2
Edward Thomson <ethomson@edwardthomson.com>
2022-02-21 13:26:00 -0500

    huh

--- src/util/futils.c
+++ src/util/futils.c
@@ -156,28 +156,29 @@ int git_futils_readbuffer_fd(git_str *buf, git_file fd, size_t len)
 	git_str_clear(buf);
 
 	if (!git__is_ssizet(len)) {
+		fprintf(stderr, "!! read too large\n");
 		git_error_set(GIT_ERROR_INVALID, "read too large");
 		return -1;
 	}
 
 	GIT_ERROR_CHECK_ALLOC_ADD(&alloc_len, len, 1);
-	if (git_str_grow(buf, alloc_len) < 0)
+	if (git_str_grow(buf, alloc_len) < 0) {
+		fprintf(stderr, "??? could not grow %" PRIuZ ": %s\n", alloc_len, strerror(errno));
 		return -1;
+	}
 
 	/* p_read loops internally to read len bytes */
 	read_size = p_read(fd, buf->ptr, len);
 
 	if (read_size < 0) {
-		printf("!!!\n");
-		perror("read");
+		fprintf(stderr, "!!! failed: %s\n", strerror(errno));
 		git_error_set(GIT_ERROR_OS, "failed to read descriptor");
 		git_str_dispose(buf);
 		return -1;
 	}
 
 	if ((size_t)read_size != len) {
-		printf("...\n");
-		perror("mismatch");
+		fprintf(stderr, "!!! mismatch: %s\n", strerror(errno));
 		git_error_set(GIT_ERROR_FILESYSTEM, "could not read (expected %" PRIuZ " bytes, read %" PRIuZ ")", len, (size_t)read_size);
 		git_str_dispose(buf);
 		return -1;
