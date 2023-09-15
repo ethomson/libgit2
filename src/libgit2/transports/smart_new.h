fd5a0b9471ecc3ffc694d12e65f5a3d08616f0d1
Edward Thomson <ethomson@edwardthomson.com>
2023-09-15 23:55:25 +0100

    checkpoint

---
+++ src/libgit2/transports/smart_new.h
@@ -0,0 +1,64 @@
+/*
+ * Copyright (C) the libgit2 contributors. All rights reserved.
+ *
+ * This file is part of libgit2, distributed under the GNU GPL v2 with
+ * a Linking Exception. For full terms see the included COPYING file.
+ */
+#ifndef INCLUDE_transports_smart_new_h__
+#define INCLUDE_transports_smart_new_h__
+
+#include "common.h"
+
+typedef enum {
+	GIT_SMART_CAPABILITY_MULTI_ACK                    = (1 <<  0),
+	GIT_SMART_CAPABILITY_MULTI_ACK_DETAILED           = (1 <<  1),
+	GIT_SMART_CAPABILITY_NO_DONE                      = (1 <<  2),
+	GIT_SMART_CAPABILITY_THIN_PACK                    = (1 <<  3),
+	GIT_SMART_CAPABILITY_SIDE_BAND                    = (1 <<  4),
+	GIT_SMART_CAPABILITY_SIDE_BAND_64K                = (1 <<  5),
+	GIT_SMART_CAPABILITY_OFS_DELTA                    = (1 <<  6),
+	GIT_SMART_CAPABILITY_AGENT                        = (1 <<  7),
+	GIT_SMART_CAPABILITY_OBJECT_FORMAT                = (1 <<  8),
+	GIT_SMART_CAPABILITY_SYMREF                       = (1 <<  9),
+	GIT_SMART_CAPABILITY_SHALLOW                      = (1 << 10),
+	GIT_SMART_CAPABILITY_DEEPEN_SINCE                 = (1 << 11),
+	GIT_SMART_CAPABILITY_DEEPEN_NOT                   = (1 << 12),
+	GIT_SMART_CAPABILITY_DEEPEN_RELATIVE              = (1 << 13),
+	GIT_SMART_CAPABILITY_NO_PROGRESS                  = (1 << 14),
+	GIT_SMART_CAPABILITY_INCLUDE_TAG                  = (1 << 15),
+	GIT_SMART_CAPABILITY_REPORT_STATUS                = (1 << 16),
+	GIT_SMART_CAPABILITY_REPORT_STATUS_V2             = (1 << 17),
+	GIT_SMART_CAPABILITY_DELETE_REFS                  = (1 << 18),
+	GIT_SMART_CAPABILITY_QUIET                        = (1 << 19),
+	GIT_SMART_CAPABILITY_ATOMIC                       = (1 << 20),
+	GIT_SMART_CAPABILITY_PUSH_OPTIONS                 = (1 << 21),
+	GIT_SMART_CAPABILITY_ALLOW_TIP_SHA1_IN_WANT       = (1 << 22),
+	GIT_SMART_CAPABILITY_ALLOW_REACHABLE_SHA1_IN_WANT = (1 << 23),
+	GIT_SMART_CAPABILITY_ALLOW_ANY_SHA1_IN_WANT       = (1 << 24),
+	GIT_SMART_CAPABILITY_PUSH_CERT                    = (1 << 25),
+	GIT_SMART_CAPABILITY_FILTER                       = (1 << 26),
+	GIT_SMART_CAPABILITY_SESSION_ID                   = (1 << 27)
+} git_smart_capability_t;
+
+#define GIT_SMART_CLIENT_CAPABILITIES \
+	(GIT_SMART_CAPABILITY_MULTI_ACK          | \
+	 GIT_SMART_CAPABILITY_MULTI_ACK_DETAILED | \
+	 GIT_SMART_CAPABILITY_NO_DONE            | \
+	 GIT_SMART_CAPABILITY_THIN_PACK          | \
+/*       GIT_SMART_CAPABILITY_SIDE_BAND          | */ \
+/*       GIT_SMART_CAPABILITY_SIDE_BAND_64K      | */ \
+	 GIT_SMART_CAPABILITY_OFS_DELTA          | \
+	 GIT_SMART_CAPABILITY_AGENT              | \
+	 GIT_SMART_CAPABILITY_OBJECT_FORMAT      | \
+	 GIT_SMART_CAPABILITY_SYMREF             | \
+	 GIT_SMART_CAPABILITY_NO_PROGRESS        | \
+	 GIT_SMART_CAPABILITY_INCLUDE_TAG        | \
+	 GIT_SMART_CAPABILITY_SESSION_ID)
+
+
+typedef struct git_smart_client git_smart_client;
+
+int git_smart_client_init(git_smart_client **out, git_repository *repo);
+void git_smart_client_free(git_smart_client *client);
+
+#endif
